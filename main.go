b3c629a1a195c23f0b4bc41e95752e34a4e83895
KingRovs771 <rizkybudiarto890@gmail.com>
2026-05-17 20:58:51 +0700

    Perbaikan Cors Fix Kayake

--- main.go
+++ main.go
@@ -55,13 +55,13 @@ func main() {
 	router := gin.Default()
 
 	router.Use(cors.New(cors.Config{
-        AllowOrigins:     []string{"https://www.mentalhealth.web.id/", "http://localhost:3000"}, // Ganti dengan domain frontendmu
-        AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
-        AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization", "X-Requested-With", "X-CSRF-Token"},
-        ExposeHeaders:    []string{"Content-Length"},
-        AllowCredentials: true,
-        MaxAge:           12 * time.Hour,
-    }))
+		AllowOrigins:     []string{"https://www.mentalhealth.web.id", "https://mentalhealth.web.id", "http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization", "X-Requested-With", "X-CSRF-Token"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	router.GET("/api/photo/getPhotoPakar/:uid", controllers.GetPakarPhoto)
 	//Index Web
