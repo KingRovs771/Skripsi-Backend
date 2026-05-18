d0a3e9f4bd77f4bdbec7cc998bedc904fb968680
KingRovs771 <rizkybudiarto890@gmail.com>
2026-05-18 07:27:22 +0700

    Perubahan Cors

--- main.go
+++ main.go
@@ -8,7 +8,9 @@ import (
 	"Skripsi-Backend/seeder"
 	"log"
 	"os"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
@@ -52,14 +54,14 @@ func main() {
 
 	router := gin.Default()
 
-	/* router.Use(cors.New(cors.Config{
+	router.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"https://www.mentalhealth.web.id", "https://mentalhealth.web.id", "http://localhost:3000"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization", "X-Requested-With", "X-CSRF-Token"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 		MaxAge:           12 * time.Hour,
-	})) */
+	}))
 
 	router.GET("/api/photo/getPhotoPakar/:uid", controllers.GetPakarPhoto)
 	//Index Web
