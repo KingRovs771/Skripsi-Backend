b545ad4427d6a9f192b00691bc7bc1f1fe98953b
KingRovs771 <rizkybudiarto890@gmail.com>
2026-05-18 07:36:31 +0700

    Perubahan Origins

--- main.go
+++ main.go
@@ -55,7 +55,7 @@ func main() {
 	router := gin.Default()
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"https://www.mentalhealth.web.id", "https://mentalhealth.web.id", "http://localhost:3000"},
+		AllowOrigins:     []string{"https://www.mentalhealth.web.id", "https://mentalhealth.web.id"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization", "X-Requested-With", "X-CSRF-Token"},
 		ExposeHeaders:    []string{"Content-Length"},
